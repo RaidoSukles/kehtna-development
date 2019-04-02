5ac5173a251a34c8d551dcf597c1be58f7e95b58
RaidoSukles <raidosukles@gmail.com>
2019-04-02 16:04:04 +0300

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -3,37 +3,48 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args)
     {
-        System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
-        int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-        int ans;
-        System.out.println("Enter your selection: 1 for Addition, 2 for Substraction 3 for Multiplication and 4 for Division:");
-        int choose;
-        choose = inp.nextInt();
+		int num1,num2;
+		String choose;
+		
+		if (args.length == 0) {
+			System.out.println("Enter first and second number:");
+			Scanner inp= new Scanner(System.in);			
+			num1 = inp.nextInt();
+			num2 = inp.nextInt();        
+			System.out.println("Enter your selection: 1 for Addition, 2 for Substraction 3 for Multiplication and 4 for Division:");        
+			choose = inp.next();
+		} else {
+			
+			num1 = Integer.parseInt(args[0]);
+			num2 = Integer.parseInt(args[1]);
+			choose = args[2];
+			// Integer.parseInt(args[0]);
+		
+			// args[0] -> num1
+			// args[2] -> num2
+			// args[1] -> choose
+			
+		}
+		
+        
         switch (choose){
-        case 1:
-            System.out.println(add( num1,num2));
-            break;
-        case 2:
-            System.out.println(sub( num1,num2));
-            break;      
-        case 3:
-            System.out.println(mult( num1,num2));
-            break;
-        case 4:
-            System.out.println(div( num1,num2));
-            break;
-            default:
-                System.out.println("Illigal Operation");
-
-
+			case "1":
+				System.out.println(add( num1,num2));
+				break;
+			case "2":
+				System.out.println(sub( num1,num2));
+				break;      
+			case "3":
+				System.out.println(mult( num1,num2));
+				break;
+			case "4":
+				System.out.println(div( num1,num2));
+				break;
+				default:
+					System.out.println("Illigal Operation");
         }
-
-
-
     }
+	
     public static int add(int x, int y)
     {
         int result = x + y;
